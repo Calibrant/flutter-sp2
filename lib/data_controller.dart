a6def9b8545ff4de96c7c39a566045b1f56cc6b5
alisher <alisher1705@gmail.com>
2021-04-02 21:28:43 +0500

    search page added

---
+++ lib/data_controller.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+
+import 'const.dart';
+
+class DataController extends GetxController {
+  Future getData(String collection) async {
+    final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    QuerySnapshot snapshot =
+        await firebaseFirestore.collection(collection).get();
+    return snapshot.docs;
+  }
+
+  Future queryData(String queryString) async {
+    return FirebaseFirestore.instance
+        .collection(COLLECTION)
+        .limit(3)
+        .where(COMPANY_SERVICE, isGreaterThanOrEqualTo: queryString)
+        .get();
+  }
+}
